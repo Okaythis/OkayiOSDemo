7634abbbbbbef4a69e6706296eb4bacefdea4e85
Ben Ogie <ben@okaythis.com>
2022-01-20 10:31:14 +0100

    Initial Commit

---
+++ OkayiOSDemo/OkayiOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkayiOSDemoApp.swift
+//  OkayiOSDemo
+//
+//  Created by Ben Ogie on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct OkayiOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
